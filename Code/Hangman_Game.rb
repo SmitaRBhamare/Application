43a241910dc7d0e13c745b0a3c1e75e9ad279f2d
SmitaRBhamare <116024743+SmitaRBhamare@users.noreply.github.com>
2023-02-03 17:19:31 +0530

    Update Hangman_Game.rb

--- Code/Hangman_Game.rb
+++ Code/Hangman_Game.rb
@@ -1,4 +1,5 @@
 
+  
   arr = ["cricket", "game", "celebrate", "chess", "safe"]
   word = arr.sample
   new_teaser = Array.new(word.length, "_")
@@ -8,28 +9,31 @@
 
   puts new_teaser.inspect 
     
-  while chances > 0 do
-    puts "Enter the letter"
-    letter = gets.chomp()
+  while chances > 0
+      puts "Enter the letter"
+      letter = gets.chomp()
 
-    if letter == "exit"
-      puts "Thank you for playing"
-    elsif letter.length > 1
-      puts "Only guess 1 letter at a time please!"
-    elsif word.include?(letter)
-      new_teaser.each_with_index do |value , index|
-        new_teaser[index] = letter if word[index] == letter
+      if letter == "exit"
+        puts "Thank you for playing"
+      elsif letter.length > 1
+        puts "Only guess 1 letter at a time please!"
+      elsif word.include?(letter)
+        new_teaser.each_with_index do |value , index|
+          new_teaser[index] = letter if word[index] == letter
+        end
+        puts new_teaser.inspect
+              
+        if word == new_teaser.join
+          puts "Congratulations... you have won this round!"
+          break
+        end
+      else   
+        chances = chances -1
+        if chances == 0
+          puts "Game over... better luck next time!"
+          break
+        end
+        puts "You have #{chances} chances" 
       end
-      puts new_teaser.inspect
-            
-      if word == new_teaser.join
-        puts "Congratulations... you have won this round!"
-        break
-      end
-    else   
-      chances = chances -1
-      puts "You have #{chances} chances"  
-    end
   end
-  puts "Game over... better luck next time!"
-
+  
